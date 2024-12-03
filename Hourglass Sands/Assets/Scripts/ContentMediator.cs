34996544066d81b3a789278fe3901a8bd37dcef8
Ian-Stentz <ianstentz25@gmail.com>
2024-12-03 13:11:56 -0800

    Button Management Framework WIP

--- Hourglass Sands/Assets/Scripts/ContentMediator.cs
+++ Hourglass Sands/Assets/Scripts/ContentMediator.cs
@@ -9,17 +9,51 @@ public class ContentMediator : MonoBehaviour
     public int rowSize = 1;
     public GameObject contentPanel;
     public GameObject contentContainerPrefab;
+    public List<GameObject> contentList = new();
+    public GameObject buttonPrefab;
+    public List<GameObject> buttons = new();
+    private ItemPool itemPool;
     void Start()
     {
+        itemPool = FindObjectOfType<ItemPoolReader>().itemPool;
         for (int i = 0; i < minRows; i++) {
             this.AddRow();
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+
+    void RefreshButtonParents() {
+        for (int i = 0; i < buttons.Count; i++) {
+            buttons[i].transform.parent = contentList[i].transform.parent;
+        }
+    }
+
+    void AddButton(int itemID) {
+        GameObject newButton = Instantiate(buttonPrefab);
+        newButton.GetComponent<ItemButton>().LoadItem(itemPool.items[itemID]);
+        int index = buttons.Count;
+        buttons.Add(newButton);
+        newButton.transform.parent = contentList[index].transform;
+    }
+
+    void InsertButton(int buttonIndex, int itemID) {
+        GameObject newButton = Instantiate(buttonPrefab);
+        newButton.GetComponent<ItemButton>().LoadItem(itemPool.items[itemID]);
+        buttons.Insert(buttonIndex, newButton);
+        RefreshButtonParents();
+    }
+
+    void RemoveButton(int buttonIndex) {
+        buttons.RemoveAt(buttonIndex);
+        RefreshButtonParents();
+    }
+
+    void ClearButtons() {
+        for(int i = 0; i < buttons.Count; i++) {
+            Destroy(buttons[i]);
+        }
+        buttons.Clear();
     }
 
     void AddRow() {
@@ -27,6 +61,7 @@ public class ContentMediator : MonoBehaviour
             for(int i = 0; i < rowSize; i++) {
                 GameObject newChild = Instantiate(contentContainerPrefab);
                 newChild.transform.SetParent(contentPanel.transform, false);
+                contentList.Add(newChild);
             }
         }
     }
---
+++ Hourglass Sands/Assets/Scripts/ItemButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadItem(GoodsItem item) {
+
+    }
+}
